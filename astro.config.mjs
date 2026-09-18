30338af9d6c7ec5ec9289a828f9b180dfa7d62f0
ItsIImery <itsilmery@gmail.com>
2026-09-18 13:52:44 +0500

    fix: update cloudflare adapter config and prerender home

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({
-    imageService: 'cloudflare',
-  }),
+  adapter: cloudflare(),
   compressHTML: true,
 });
